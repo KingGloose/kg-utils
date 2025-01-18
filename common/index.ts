f2b5d69409098852a19fec8fa92a51606d1bfab2
zhangjiahui <2319671152@qq.com>
2025-01-18 12:18:56 +0800

    fix: 新增 useAnimationFrame 函数

--- common/index.ts
+++ common/index.ts
@@ -24,7 +24,7 @@ export function deepClone(value: any, type = CLONE_MODE.AUTO) {
       return oriValue;
     }
 
-    // 解决循环引用
+    // 循环引用
     if (map.has(oriValue)) {
       return map.get(oriValue);
     }
@@ -99,4 +99,37 @@ export function deepClone(value: any, type = CLONE_MODE.AUTO) {
   }
 
   return cloneMethod(value);
+}
+
+/**
+ * 创建一个动画帧控制器，允许启动和停止基于时间的动画。
+ *
+ * @param animationFn - 一个回调函数，接收一个表示动画进度的参数（范围从 0 到 1）。
+ * @param animationTime - 动画的总持续时间（以毫秒为单位）。默认值为 0，表示动画会一直进行直到手动停止。
+ * @returns 一个对象，包含两个方法：
+ *   - `start`: 启动动画。
+ *   - `stop`: 停止动画。
+ */
+export function useAnimationFrame(animationFn: (progress: number) => void, animationTime = 0): { start: any; stop: any } {
+  let isFinish = false;
+
+  const start = () => {
+    let startTime: number = 0;
+    const animation = (currentTime: DOMHighResTimeStamp) => {
+      if (!startTime) startTime = currentTime;
+      const progress = (currentTime - startTime) / animationTime;
+      if (!isFinish && progress <= 1) {
+        animationFn(progress);
+        requestAnimationFrame(animation);
+      }
+    };
+
+    requestAnimationFrame(animation);
+  };
+
+  const stop = () => {
+    isFinish = true;
+  };
+
+  return { start, stop };
 }
