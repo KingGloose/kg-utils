b163efaed249d8c74d8a167bf01f968940cff563
zhangjiahui <2319671152@qq.com>
2025-01-29 01:24:12 +0800

    feat: 添加 color

---
+++ utils/color/color.ts
@@ -0,0 +1,7 @@
+export function HexToRgba(hexColor: string, alpha: number = 1) {
+  hexColor = hexColor.replace("#", "");
+  const r = parseInt(hexColor.substring(0, 2), 16);
+  const g = parseInt(hexColor.substring(2, 4), 16);
+  const b = parseInt(hexColor.substring(4, 6), 16);
+  return `rgba(${r},${g},${b},${alpha})`; // 返回 RGBA 格式
+}
